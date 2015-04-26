96cd1d45b6cf651347a927a3bc53148dbc6432cb
gisfromscratch <gisfromscratch@live.de>
2015-04-26 19:01:49 +0200

    Access the root path and log using commons logging.

--- samples.mapreduce/src/main/java/edu/hadoop/gis/samples/mapreduce/App.java
+++ samples.mapreduce/src/main/java/edu/hadoop/gis/samples/mapreduce/App.java
@@ -1,13 +1,25 @@
 package edu.hadoop.gis.samples.mapreduce;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
 /**
- * Hello world!
+ * Sample application using the File System API
+ * 
+ * @author Jan Tschada
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	private static final Log logger = LogFactory.getLog(App.class);
+	
+	public static void main(String[] args) {
+		logger.info("Reading HDFS content...");
+		
+		Path rootPath = new Path("/");
+		boolean isRoot = rootPath.isRoot();
+		logger.info(String.format("%s root: %b", rootPath.getName(), isRoot));
+	}
 }
