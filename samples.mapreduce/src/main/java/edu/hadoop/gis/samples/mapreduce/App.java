bba9db7dac046b78d5b79c7c7b1f6c01ce90fac8
gisfromscratch <gisfromscratch@live.de>
2015-04-26 20:50:33 +0200

    Simple call of Open Shapefile dataset on the local file system.

--- samples.mapreduce/src/main/java/edu/hadoop/gis/samples/mapreduce/App.java
+++ samples.mapreduce/src/main/java/edu/hadoop/gis/samples/mapreduce/App.java
@@ -10,6 +10,10 @@ import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.mapreduce.Job;
+import org.gdal.gdal.gdal;
+import org.gdal.ogr.DataSource;
+import org.gdal.ogr.Layer;
+import org.gdal.ogr.ogr;
 
 /**
  * Sample application using the File System API
@@ -20,6 +24,22 @@ import org.apache.hadoop.mapreduce.Job;
 public class App {
 	
 	private static final Log logger = LogFactory.getLog(App.class);
+	private static final String ShapeFileExtenstion = ".shp";
+	
+	private static void readShapefile() {
+		logger.info("Registering gdal drivers...");
+		gdal.AllRegister();
+		logger.info("Registering ogr drivers...");
+		ogr.RegisterAll();
+		
+		DataSource datasource = ogr.Open("~/Downloads/Geodata/buildings.shp", false);
+		int layerCount = datasource.GetLayerCount();
+		logger.info(String.format("Datasource '%s' containing '%d' layers.", datasource.GetName(), layerCount));
+		for (int layerIndex = 0; layerIndex < layerCount; layerIndex++) {
+			Layer layer = datasource.GetLayer(layerIndex);
+			logger.info(String.format("Reading layer '%s'.", layer.GetName()));
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		Configuration configuration = new Configuration();
@@ -34,7 +54,11 @@ public class App {
 		RemoteIterator<LocatedFileStatus> files = fileSystem.listFiles(rootPath, true);
 		while (files.hasNext()) {
 			LocatedFileStatus file = files.next();
-			logger.info(String.format("Path: '%s'", file.getPath()));
+			Path filePath = file.getPath();
+			logger.info(String.format("Path: '%s'", filePath));
 		}
+		
+		readShapefile();
+		logger.info("Done...");
 	}
 }
