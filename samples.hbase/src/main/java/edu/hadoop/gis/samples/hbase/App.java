8684082ca4e2f5d2e41031b834918fb5eae918ff
J.T <gisfromscratch@live.de>
2016-05-22 10:02:17 +0200

    Added the geonames sample file and the console app arguemnts parsing.

--- samples.hbase/src/main/java/edu/hadoop/gis/samples/hbase/App.java
+++ samples.hbase/src/main/java/edu/hadoop/gis/samples/hbase/App.java
@@ -1,13 +1,55 @@
 package edu.hadoop.gis.samples.hbase;
 
+import java.io.File;
+
 /**
- * Hello world!
+ * Simple HBase client for importing text files into HBase tables.
+ * 
+ * @author Jan Tschada
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		File inputFile = null;
+		String tableName = null;
+		boolean readKey = true;
+		AppTypeArgument argumentType = AppTypeArgument.Unknown;
+		for (String arg : args) {
+			if (readKey) {
+				if ("-CSV".equalsIgnoreCase(arg)) {
+					argumentType = AppTypeArgument.InputFile;
+				} else if ("-TABLE".equalsIgnoreCase(arg)) {
+					argumentType = AppTypeArgument.TableName;
+				}
+			} else {
+				switch (argumentType) {
+				case InputFile:
+					inputFile = new File(arg);
+					break;
+				case TableName:
+					tableName = arg;
+					break;
+				}
+			}
+			readKey = !readKey;
+		}
+		
+		if (null == inputFile || null == tableName) {
+			System.err.println("Usage: -csv <input_file> -table <hbase-table>");
+			return;
+		}
+		
+		
+	}
+
+	/**
+	 * The argument types of this client app.
+	 * 
+	 * @author Jan Tschada
+	 *
+	 */
+	private enum AppTypeArgument {
+		Unknown, InputFile, TableName
+	}
 }
